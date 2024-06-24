70b074775e1eb638e0994d58d1c136497c7e7aa0
chaim <chaim.triebitz@bhbsdventures.com>
2024-06-24 17:59:56 +0300

    monday

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { svgs } from './assets/svgs'
+import logo from './assets/imgs/logo-transparent.png'
+import headerBanner from './assets/imgs/1.png'
 
 function App() {
 
    return (
       <div className="App" >
-         <header className='header'></header>
+         <header className='header'>
+            <img className='banner' src={headerBanner} alt="" />
+            <section className="section txt-section">
+               <div className="img">
+                  <img className='logo' src={logo} alt="logo" />
+               </div>
+
+               <h1>YOUR DREAM APARTMENT</h1>
+               <span className='rec'></span>
+               <h1>in the pulsing heart of Yerushalayim</h1>
+            </section>
+         </header>
          <main className='main'></main>
          <footer className='footer'>
             <p>For more information,</p>
