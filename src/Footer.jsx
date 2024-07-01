0121ea1419359143e2bd13862c09bc3d770dadd7
chaim triebitz <ctriebitz@gmail.com>
2024-07-02 02:10:03 +0300

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -12,16 +12,15 @@ export const Footer = () => {
                href={`tel:6179554404`}
                title={'call me'}
             >
-               {svgs.phone}
+               <span className="svg-animate">{svgs.phone}</span>
                <p>(617) 955-4404</p>
                <span>|</span>
             </a>
             <a className="item"
                href="https://wa.me/972544978899?text=meah%20shearim"
                title={'message me'}
-
             >
-               {svgs.whatsapp}
+               <span className="svg-animate">{svgs.whatsapp}</span>
                <p>+972 544 978899</p>
                <span>|</span>
             </a>
@@ -30,10 +29,11 @@ export const Footer = () => {
                href={`mailto:a@volk.co.il`}
                title={'mail me'}
             >
-               {svgs.mail}
+               <span className="svg-animate">{svgs.mail}</span>
                <p>a@volk.co.il</p>
             </a>
          </div>
       </footer>
    )
 }
+
